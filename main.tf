3895dbba7037571e7459c25974f70eacb2ad391c
Xavier Venteo <xavierventeo@gmail.com>
2024-05-28 23:12:53 +0200

    Enable rds logs and send to cloudwatch

--- main.tf
+++ main.tf
@@ -46,6 +46,51 @@ resource "aws_instance" "windowserver_instance" {
     Name = "Lab WindowsServer Instance"
   }
 }
+##########################
+
+# Create an RDS parameter group to enable CloudWatch logging
+resource "aws_db_parameter_group" "lab_db_parameter_group" {
+  name        = "lab-db-parameter-group"
+  family      = "mysql8.0"
+  description = "Custom parameter group for RDS MySQL with CloudWatch logging"
+
+  parameter {
+    name  = "slow_query_log"
+    value = "1"
+  }
+  #slow_query_log = 1 or log_slow_query = 1
+
+  parameter {
+    name  = "general_log"
+    value = "1"
+  }
+
+  parameter {
+    name  = "log_output"
+    value = "FILE"
+  }
+}
+
+#Create an option group to enable the MariaDB audit plugin
+resource "aws_db_option_group" "lab_db_option_group" {
+  name                     = "lab-db-option-group"
+  engine_name              = "mysql"
+  major_engine_version     = "8.0"
+  option_group_description = "Option group for enabling MariaDB audit plugin"
+
+  option {
+    option_name = "MARIADB_AUDIT_PLUGIN"
+    option_settings {
+      name  = "SERVER_AUDIT_EVENTS"
+      value = "CONNECT,QUERY,QUERY_DDL,QUERY_DML,QUERY_DML_NO_SELECT,QUERY_DCL"
+    }
+  }
+  tags = {
+    Name = "lab_db_option_group"
+  }
+}
+
+
 
 # RDS Instance creation
 resource "aws_db_instance" "db_instance" {
@@ -62,6 +107,9 @@ resource "aws_db_instance" "db_instance" {
   skip_final_snapshot    = var.database_settings.mysql.skip_final_snapshot
   db_subnet_group_name   = aws_db_subnet_group.database_subnet_group.name
   vpc_security_group_ids = [aws_security_group.database_sg.id]
+  parameter_group_name = aws_db_parameter_group.lab_db_parameter_group.name
+  option_group_name    = aws_db_option_group.lab_db_option_group.name
+  enabled_cloudwatch_logs_exports = ["slowquery", "error", "general", "audit"]
   tags = {
     Name = "Lab Database"
   }
